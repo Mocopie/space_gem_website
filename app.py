05a0f715fc645a951644d6c26927399103ceb845
Mocopie <sami.benhassine@googlemail.com>
2025-03-18 11:15:14 +0100

    slight mod to app.py - first step

--- app.py
+++ app.py
@@ -1,53 +1,47 @@
 import streamlit as st
 
-"""
-# Space Gem Front
-"""
-
-st.markdown(
-    """
-Remember that there are several ways to ouput content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph usin gthe `st.` functions.
-"""
-)
-
+import datetime
+import requests
 
 """
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
+# space gem front
 
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
+This front queries the space gem api [space gem model API](https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2)
 """
 
-"""
-## Once we have these, let's call our API in order to retrieve a prediction
+with st.form(key="params_for_api"):
 
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-"""
-
-url = "https://taxifare.lewagon.ai/predict"
-
-if url == "https://taxifare.lewagon.ai/predict":
-
-    st.markdown(
-        "Maybe you want to use your own API for the prediction, not the one provided by Le Wagon..."
+    pickup_date = st.date_input(
+        "pickup datetime", value=datetime.datetime(2012, 10, 6, 12, 10, 20)
+    )
+    pickup_time = st.time_input(
+        "pickup datetime", value=datetime.datetime(2012, 10, 6, 12, 10, 20)
+    )
+    pickup_datetime = f"{pickup_date} {pickup_time}"
+    pickup_longitude = st.number_input("pickup longitude", value=40.7614327)
+    pickup_latitude = st.number_input("pickup latitude", value=-73.9798156)
+    dropoff_longitude = st.number_input("dropoff longitude", value=40.6413111)
+    dropoff_latitude = st.number_input("dropoff latitude", value=-73.7803331)
+    passenger_count = st.number_input(
+        "passenger_count", min_value=1, max_value=8, step=1, value=1
     )
 
-"""
+    st.form_submit_button("Make prediction")
 
-2. Let's build a dictionary containing the parameters for our API...
+params = dict(
+    pickup_datetime=pickup_datetime,
+    pickup_longitude=pickup_longitude,
+    pickup_latitude=pickup_latitude,
+    dropoff_longitude=dropoff_longitude,
+    dropoff_latitude=dropoff_latitude,
+    passenger_count=passenger_count,
+)
 
-3. Let's call our API using the `requests` package...
+wagon_cab_api_url = "https://taxifare.lewagon.ai/predict"
+response = requests.get(wagon_cab_api_url, params=params)
 
-4. Let's retrieve the prediction from the **JSON** returned by the API...
+prediction = response.json()
 
-## Finally, we can display the prediction to the user
-"""
+pred = prediction["fare"]
+
+st.header(f"Fare amount: ${round(pred, 2)}")
