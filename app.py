7be2bd8f9c9ae63d703b5dd7510110f1d1312e1f
Mocopie <sami.benhassine@googlemail.com>
2025-03-20 11:44:56 +0100

    added important to css

--- app.py
+++ app.py
@@ -258,11 +258,12 @@ if img_file_buffer is not None:
 
     with col2:
         img_bytes = img_file_buffer.getvalue()
-        with st.markdown(
-            '<div class="spinner-container">', unsafe_allow_html=True
-        ):
-            with st.spinner("✨ Analyzing the gemstone..."):
-                result, error = detect_gemstones(img_bytes)
+        st.markdown("<div class='main'>", unsafe_allow_html=True)
+        # with st.markdown(
+        #     '<div class="spinner-container">', unsafe_allow_html=True
+        # ):
+        with st.spinner("✨ Analyzing the gemstone..."):
+            result, error = detect_gemstones(img_bytes)
             # Close the spinner div here after the spinner completes
         st.markdown("</div>", unsafe_allow_html=True)
 
